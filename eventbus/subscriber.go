244475e936860d2544ad407763eaef3eecaa33d1
WardonNE <1730314864@qq.com>
2023-09-25 11:10:34 +0800

    update

--- eventbus/subscriber.go
+++ eventbus/subscriber.go
@@ -1,5 +1,7 @@
 package eventbus
 
+// Subscriber is subscriber interface
 type Subscriber interface {
+	// Subscribe accepts an instance of IEventBus and returns listener map
 	Subscribe(eb IEventBus) map[string][]ListenerClause
 }
