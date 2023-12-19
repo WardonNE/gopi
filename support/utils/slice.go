bbdab8cfcb7e26ae3ced60738bc2e6317c75c4cb
WardonNE <1730314864@qq.com>
2023-12-19 11:49:51 +0800

    add array diff func

--- support/utils/slice.go
+++ support/utils/slice.go
@@ -14,3 +14,18 @@ func Max[T constraints.Ordered](values ...T) T {
 func Min[T constraints.Ordered](values ...T) T {
 	return slices.Min(values)
 }
+
+// Diff return the elements in a that are not in b
+func Diff[T constraints.Ordered](a []T, b []T) []T {
+	diff := []T{}
+	bSet := map[T]struct{}{}
+	for _, item := range b {
+		bSet[item] = struct{}{}
+	}
+	for _, x := range a {
+		if _, ok := bSet[x]; !ok {
+			diff = append(diff, x)
+		}
+	}
+	return diff
+}
